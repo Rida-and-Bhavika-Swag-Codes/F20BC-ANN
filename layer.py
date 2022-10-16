ab005a9cc1ffaa8f89f0210f6893cbe674aaf6be
Bhavika <bk2002@hw.ac.uk>
2022-10-17 02:49:30 +0400

    undo changes

--- layer.py
+++ layer.py
@@ -29,8 +29,7 @@ class Layer:
 
     """Use with backpropogation"""
     def update_param(self, learning_rate, gradients):
-        self.weights -= learning_rate * gradients
-        self.bias -= learning_rate * gradients
+        self.weights = self.weights - learning_rate * gradients
 
     """debug function"""
     def get_properties(self):
