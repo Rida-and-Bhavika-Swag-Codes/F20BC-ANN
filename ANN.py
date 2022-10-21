fixed backprop error

ANN.py
@@ -36,7 +36,7 @@ class ANN:
             print("propogating forward")
             for sample in range (len(self.input[0])):
                 print("the current sample is", sample)
-                layer.Layer.propogate_forward(self.layers[0],self.input[:,sample])
+                layer.Layer.propogate_forward(self.layers[0], self.input[:,sample])
                 for i in range(len(self.layers)-2):
                     print("propogating forward hidden layers")
                     layer.Layer.propogate_forward(self.layers[i+1], self.layers[i].output)
@@ -99,13 +99,22 @@ class ANN:
         total_loss = 0
         
         #the predicted outcome
-        pred = self.layers[-1].input.T
+        pred = self.layers[-1].input
         print(len(self.output))
         print("the calculated outputs are", pred)
+        print("the true outputs are", Y)
         error = self.loss_prime(Y, pred)
         print("the calculated error is", error)
-        for layer in reversed(self.layers[:-1]):
+        #for layer in reversed(self.layers[:-1]):
+        print("THE LAYERS", self.layers)
+        print("THE SLICED LAYERS: ", self.layers[1:-1])
+        for layer in reversed(self.layers[1:-1]):
             error = layer.propogate_backward(error, self.learning_rate)
+        #backpropogate last layer
+        
+        self.layers[0].input.resize(1,30) 
+        print("new input shape", self.layers[0].input.shape)
+        error = self.layers[0].propogate_backward(error, self.learning_rate) 
 
         """
         for p, y in zip(pred, Y):
@@ -137,7 +146,7 @@ def mse(pred, y):
     return np.mean(np.power(y - pred, 2))
 
 def dmse(pred, y):
-    return 2*(pred-y)/y.shape[0]
+    return 2*(pred-y)/y.size
 
 
 # adding epsilon to the predicted output to avoid log(0) error

layer.py
@@ -16,8 +16,9 @@ class Layer:
         self.bias = np.random.rand(1,output_connections) - 0.5     
 
     def propogate_forward(self, input):
+        print("the input to forward prop", self.input)
         #set input vector
-        self.input = input
+        self.input = np.array(input)
         # calculate weighted sum
         wsum = np.dot(input,self.weights) + self.bias
         # convert type to float32 [reference: https://stackoverflow.com/questions/18557337/numpy-attributeerror-float-object-has-no-attribute-exp]
@@ -26,6 +27,7 @@ class Layer:
         # apply activation
         if self.activation: #if self.activation null then this is an output layer and we don't forward propogate from here
             self.output = self.activation(wsum)
+            print("the calculated output", self.output)
             return self.output
 
     def propogate_backward(self, loss, learning_rate):
@@ -37,7 +39,11 @@ class Layer:
         #ierror = loss * self.weights.T # input error
         
         input_error = np.dot(loss, self.weights.T)
+        print("shape of input", self.input.T.shape)
+        print("shape of loss", loss.shape)
         weights_error = np.dot(self.input.T, loss)
+        
+
         #wgrad = np.dot(self.input.T, loss)
         #bgrad = loss * 1
 