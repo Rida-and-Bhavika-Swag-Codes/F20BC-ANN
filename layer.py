fixing exploding gradients problem

layer.py
@@ -16,9 +16,11 @@ class Layer:
         self.bias = np.random.rand(1,output_connections) - 0.5     
 
     def propogate_forward(self, input):
-        print("the input to forward prop", self.input)
         #set input vector
         self.input = np.array(input)
+        print("the input is ", input)
+        print("the weights are ", self.weights)
+
         # calculate weighted sum
         wsum = np.dot(input,self.weights) + self.bias
         # convert type to float32 [reference: https://stackoverflow.com/questions/18557337/numpy-attributeerror-float-object-has-no-attribute-exp]
@@ -27,7 +29,6 @@ class Layer:
         # apply activation
         if self.activation: #if self.activation null then this is an output layer and we don't forward propogate from here
             self.output = self.activation(wsum)
-            print("the calculated output", self.output)
             return self.output
 
     def propogate_backward(self, loss, learning_rate):