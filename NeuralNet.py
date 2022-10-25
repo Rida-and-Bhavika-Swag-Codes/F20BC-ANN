8ba77d4ac2a04abb97ead8e11f09710a6fa4f922
Rida <rida.aqp@gmail.com>
2022-10-26 02:18:49 +0400

    updates to forward and back prop

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,4 +1,5 @@
 import layer
+import numpy as np
 class ANN:
 
     """
@@ -18,11 +19,11 @@ class ANN:
         self.loss_function = None
         self.loss_prime = None
         # 1 = bce, 2 = mse, 3 = mae
-        match loss:
-            case 1 : self.loss_function, self.loss_prime = bce, dbce
-            case 2 : self.loss_function, self.loss_prime = mse, dmse
-            case 3 : self.loss_function, self.loss_prime = mae, dmae
-            case other: print("no loss function selected")
+        # match loss:
+        #     case 1 : self.loss_function, self.loss_prime = bce, dbce
+        #     case 2 : self.loss_function, self.loss_prime = mse, dmse
+        #     case 3 : self.loss_function, self.loss_prime = mae, dmae
+        #     case other: print("no loss function selected")
 
     def setLayers(self, activations, *nodes_per_layer):
         print("the activations are", activations)
@@ -34,18 +35,19 @@ class ANN:
         #append input layer
         #l = layer.Layer(len(self.input), nodes_per_layer[0], activations[0])
         l = layer.Layer(30, 10, activations[0])
+        l.input = self.input
         self.layers.append(l)
         print("adding input layer")
 
         #append hidden layers
         for i in range (len(nodes_per_layer)-1):
             #self.layers.append(layer.Layer(nodes_per_layer[i], nodes_per_layer[i+1], activations[i+1]))
-            l = layer.Layer(10, 2, activations[0])
+            self.layers.append(layer.Layer(nodes_per_layer[i], nodes_per_layer[i+1], activations[i+1]))
             print("added 1 hidden layer")
 
         #append output layer
         #self.layers.append(layer.Layer(nodes_per_layer[-1], 0, 0 ))#output layer has no output connections or activation function
-        self.layers.append(layer.Layer(2, 0, 0 ))
+        self.layers.append(layer.Layer(nodes_per_layer[-1], 0, 0 ))
         print("added 1 output layer")
 
     def propogate_forward(self):
@@ -54,17 +56,59 @@ class ANN:
         # wsum = first_layer.weights.dot(self.input) + first_layer.bias
         # first_layer.output = first_layer.activation(2)(wsum)
 
-
+        print("the network layers are", self.layers)
         for currlayer, nextlayer in zip(self.layers[:-1], self.layers[1:]):
             print("weights", currlayer.weights)
-            print("the activations of this layer", currlayer.input)
+            print("the activations of this layer", currlayer.activation)
             #calculate weighted sum
-            wsum = currlayer.weights.dot(currlayer.input) + currlayer.bias
+            currlayer.wsum = currlayer.weights.dot(currlayer.input) + currlayer.bias
             #apply activation
-            currlayer.output = currlayer.activation(2)(wsum)
+            currlayer.output = currlayer.activation(currlayer.wsum)
             #set next layers input as the output frm this layer
             nextlayer.input = currlayer.output
-            
+
+
+    def propogate_backward(self):
+        wgrad, bgrad = [], []
+        one_hot_Y = one_hot(self.output)
+        layer = self.layers[-2]
+        error = layer.output - one_hot_Y
+        m = self.output.size
+        
+        for i in range(-2, -(len(self.layers)), -1):
+           smn = 1/ m * error.dot(self.layers[i].input.T)
+           wgrad.append(smn)
+           print("the layer is", layer.weights.shape)
+           print("the w of the last layer", smn.shape)
+           smn = 1/m *np.sum(error)
+           bgrad.append(smn)
+           print("the b of the last layer", smn.shape)
+           error = layer.weights.T.dot(error) * self.layers[i-1].activation_prime(self.layers[i-1].wsum)
+
+        #return wgrad, bgrad
+        self.parameter_update(wgrad, bgrad)
+
+    def parameter_update(self, wgrad, bgrad):
+        for layer, wupdate, bupdate in zip(reversed(self.layers), wgrad, bgrad):
+            print("dimensions", layer.weights.shape, wupdate.shape, bupdate.shape)
+            layer.weights -= (self.learning_rate * wupdate)
+            layer.bias -= (self.learning_rate * bupdate)
+            print("new weights", layer.weights)
+            print("new bias", layer.bias)
+        
+        
+        # #dZ2 = A2 - one_hot_Y
+        # dZ2 = mse(A2, Y)
+        # dZ2 = A2 - Y
+        # print("OUR ONE HOT ENCODED TRUE DATA", Y)
+        # print("Our output is", A2)
+        # print("LOSS IS", dZ2)
+        # dW2 = 1 / m * dZ2.dot(A1.T)
+        # db2 = 1 / m * np.sum(dZ2)
+        # dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+        # dW1 = 1 / m * dZ1.dot(X.T)
+        # db1 = 1 / m * np.sum(dZ1)
+        # return dW1, db1, dW2, db2     
 
 
     def train(self):
@@ -77,6 +121,13 @@ class ANN:
             #     predictions = get_predictions(self.layers[-1].input)
             #     print("Accuracy ", get_accuracy(predictions, self.output))
 
+
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
 
     
 
