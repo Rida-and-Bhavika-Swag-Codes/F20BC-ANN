fixing errors in loss function and backprop implementation

NeuralNet.py
@@ -18,8 +18,8 @@ class ANN:
 
         match loss:
             case 1 : self.loss_function = bce
-            case 2 : self.loss_function = sigmoid_loss
-            #case 3 : self.loss_function = mae            
+            #case 2 : self.loss_function = sigmoid_loss
+            case 3 : self.loss_function = mse #mae            
             case other: self.loss_function = None
  
 
@@ -54,6 +54,7 @@ class ANN:
             print("weights", currlayer.weights)
             #calculate weighted sum
             currlayer.wsum = currlayer.weights.dot(currlayer.input) + currlayer.bias
+            print("the curr layers wsum", currlayer.wsum)
             #apply activation
             currlayer.output = currlayer.activation(currlayer.wsum)
             #set next layers input as the output frm this layer
@@ -62,6 +63,8 @@ class ANN:
 
     def propogate_backward(self):
         wgrad, bgrad = [], []
+        m = self.output.size
+
         #use one hot encoding with 2 output nodes
         if self.layers[-1].num_nodes != 1:
             true_Y = one_hot(self.output)
@@ -70,35 +73,30 @@ class ANN:
 
         layer = self.layers[-2]
         #error = layer.output - true_Y
-        error = self.loss_function(layer.output, true_Y)
-        print("initial error shape", error.shape, "output shape", layer.output.shape, "encoded Y", true_Y.shape)
-        print("my error function", self.loss_function)
-
+        print("the last layers wsum", layer.output)
+        print("the last layers output", layer.output)
+        error =  self.loss_function(layer.output, true_Y) * layer.activation_prime(layer.wsum)
+        #error = np.full((2, 469), self.loss_function(layer.output, true_Y))
         
-        m = self.output.size
         
         for i in range(-2, -(len(self.layers)+1), -1):
-           #smn = 1/ m * error.dot(self.layers[i].input.T)
+           print("propogating back")
            smn = (1/ m) * np.dot(error, self.layers[i].input.T)
            #print("appending the change for wgrad", wgrad)
            wgrad.append(smn)
-           smn = 1/m *np.sum(error)
+           smn = (1/m) *np.sum(error)
            bgrad.append(smn)
            #back propogate first layer out of the loop
            if i == -(len(self.layers)):
             break
-           #error = np.dot((self.layers[i].weights).T,error) 
-           error = self.layers[i].weights.T.dot(error) 
-           print("error shape is", error.shape)
+           error = np.dot((self.layers[i].weights).T,error) 
            error = error * self.layers[i-1].activation_prime(self.layers[i-1].wsum)
-           print("acitvation p shape is", self.layers[i-1].activation_prime(self.layers[i-1].wsum).shape)
         
 
         self.parameter_update(wgrad, bgrad)
 
     def parameter_update(self, wgrad, bgrad):
         for layer, wupdate, bupdate in zip(reversed(self.layers[:-1]), wgrad, bgrad):
-            print("dimensions", layer, layer.weights.shape, wupdate.shape, bupdate.shape)
             layer.weights = layer.weights - (self.learning_rate * wupdate)
             layer.bias = layer.bias - (self.learning_rate * bupdate)
              
@@ -131,15 +129,25 @@ def one_hot(Y):
 # adding epsilon to the predicted output to avoid log(0) error and for stability
 EPSILON = 1e-7  
 
-def bce(pred, y):
-    print("the error is", -np.mean((y * np.log(pred + EPSILON)) + (1 - y ) * np.log(1 - pred + EPSILON)))
-    return -np.mean((y * np.log(pred + EPSILON)) + (1 - y ) * np.log(1 - pred + EPSILON))
-def sigmoid_loss(pred, y):
-    return np.sum(np.multiply(y,np.log(pred)) + np.multiply((1-y),np.log(1-pred)))
-# def mse(pred, y):
-#     print("first step substract", y - pred)
-#     print("step 2 square", )
-#     return np.square(np.subtract(y,pred)).mean()
+# def bce(pred, y):
+#     print("the error is", -((y * np.log(pred + EPSILON)) + (1 - y ) * np.log(1 - pred + EPSILON)))
+#     return -((y * np.log(pred + EPSILON)) + (1 - y ) * np.log(1 - pred + EPSILON))
+
+def bce(y_pred,y_true): #https://stackoverflow.com/questions/67615051/implementing-binary-cross-entropy-loss-gives-different-answer-than-tensorflows
+    y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+    term_0 = (1-y_true) * np.log(1-y_pred + 1e-7)
+    term_1 = y_true * np.log(y_pred + 1e-7)  
+    print("the error is",  term_0+term_1)
+    return -np.mean(term_0+term_1)
+
+# def sigmoid_loss(pred, y):
+#     print("sigmoid loss is", np.sum(np.multiply(y,np.log(pred)) + np.multiply((1-y),np.log(1-pred))))
+#     return np.sum(np.multiply(y,np.log(pred)) + np.multiply((1-y),np.log(1-pred)))
+
+def mse(pred, y):
+    print("first step substract", y - pred)
+    print("step 2 square", )
+    return np.square(np.subtract(y,pred)).mean()
 
 # def mae(pred, y):
 #     return np.mean(abs(y - pred))